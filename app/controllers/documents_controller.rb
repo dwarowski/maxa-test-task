fbf4d4d95435fab605cebc715f712d13aa7b80f8
Dwarowski <realcarenthusiast@yandex.ru>
2025-08-22 23:33:26 +0300

    add: serivce and controller for watermark

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,19 +1,16 @@
 class DocumentsController < ApplicationController
   def create 
     svg_file = params[:file] ## Откуда взялся params 
-    unless svg_file
-      render json: {error: "file not upload"}, status: :bad_request # Кострукция непонтная 
-      return 
-    end
+    return render json: {error: "file not upload"}, status: :bad_request unless svg_file
 
-    tmp_path = Rails.root.join("tmp", svg_file.original_filename)
-    File.binwrite(tmp_path, svg_file.read)
+    svg_content = svg_file.read
 
-    service = AddWatermarkService.new(tmp_path, text: "amerikaya hallo!")
-    svg_doc = service.call
-    out_path = tmp_path.sub_ext(".svg")
-    svg_doc.save(out_path)
+    service = AddWatermarkService.new(svg_content, text: "amerikaya hallo!")
+    service.call
 
-    render json: {url: "/download/#{out_path.basename}"}
+    out_path = Rails.root.join("public", "uploads", "watermarked.svg")
+    service.save(out_path)
+
+    render json: {url: "#{request.base_url}/upload/watermarked.svg"}
   end
 end
