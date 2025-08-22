c3158fa802af5899b84166802f1cf5954e89237f
Dwarowski <realcarenthusiast@yandex.ru>
2025-08-22 23:51:58 +0300

    add: dir separation, orig_file save and naming saves after mark

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,16 +1,24 @@
 class DocumentsController < ApplicationController
   def create 
+    upload_dir = Rails.root.join("public", "uploads")
+    download_dir = Rails.root.join("public", "downloads")
+    FileUtils.mkdir_p([upload_dir, download_dir])
+
+
     svg_file = params[:file] ## Откуда взялся params 
     return render json: {error: "file not upload"}, status: :bad_request unless svg_file
 
     svg_content = svg_file.read
+    filename = File.basename(svg_file.original_filename, ".*")
+    filepath = upload_dir.join(filename.to_s)
+
+    File.open(filepath, "wb") { |f| f.write(svg_content) }
 
     service = AddWatermarkService.new(svg_content, text: "amerikaya hallo!")
     service.call
 
-    out_path = Rails.root.join("public", "uploads", "watermarked.svg")
-    service.save(out_path)
+    service.save(download_dir.join("#{filename}_marked.svg"))
 
-    render json: {url: "#{request.base_url}/upload/watermarked.svg"}
+    render json: {url: "#{request.base_url}/download/#{filename}_marked.svg"}
   end
 end
