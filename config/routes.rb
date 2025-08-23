9d3c7a69338a1b35bdbcb8a2e4d47220059c8bcf
Dwarowski <realcarenthusiast@yandex.ru>
2025-08-23 20:10:21 +0300

    add: rswag to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs'
   
   root "pages#home"
   resource :documents, only: [:create]
