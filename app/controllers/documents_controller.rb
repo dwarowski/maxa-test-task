82382c83516aab6dc5d26494dab4ef46cd8ca248
Dwarowski <realcarenthusiast@yandex.ru>
2025-08-24 19:45:38 +0300

    refactor: documented

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,26 +1,32 @@
 class DocumentsController < ApplicationController
+  
+  # Documents endpoint
+  #
+  # @return [json] 
+  # On succsess {url: http://example.org/}
+  # On failure {error: description, status: 400}
   def create 
-    #create folders
+    # Create basic folders
     upload_dir = Rails.root.join("public", "uploads")
     download_dir = Rails.root.join("public", "downloads")
     FileUtils.mkdir_p([upload_dir, download_dir])
 
-    #get file
+    # Get file from multipart
     file = params[:file]
-    return render json: {error: "file not upload"}, status: :bad_request unless file && file.respond_to?(:original_filename)
+    return render json: {error: "File not uploaded"}, status: :unprocessable_entity unless file && file.respond_to?(:original_filename)
 
-    #file essentials
+    # File essentials
     file_content = file.read
     if !file.original_filename.match(/\.svg/)
-      return render json: { error: "unsupported file format" }, status: :bad_request
+      return render json: { error: "Unsupported file format" }, status: :unsupported_media_type
     end 
     filename = File.basename(file.original_filename, ".*")
     filepath = upload_dir.join("#{filename}.svg")
     
-    #save upload
+    # Save uploaded file
     File.open(filepath, "wb") { |f| f.write(file_content) }
 
-    #convert and mark
+    # Convert and mark and check for correct SVG
     begin 
       service = SvgToPdfService.new(file_content, watermark: "MAXA-test-task")
       service.save(download_dir.join("#{filename}_marked.pdf"))
@@ -29,7 +35,6 @@ class DocumentsController < ApplicationController
       return render json: { error: "Invalid SVG file"}, status: :bad_request
     end
 
-    #return
     render json: {url: "#{request.base_url}/downloads/#{filename}_marked.pdf"}
   end
 end
