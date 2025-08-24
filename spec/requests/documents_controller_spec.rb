1ae2bedaf5f1bafd6e0e4ee99509575937ed642c
Dwarowski <realcarenthusiast@yandex.ru>
2025-08-24 20:47:12 +0300

    refactor: renamed and documented

--- spec/requests/documents_controller_spec.rb
+++ spec/requests/documents_controller_spec.rb
@@ -4,10 +4,12 @@ require "rails_helper"
 
 describe "Documents_API", type: :request do
   describe "POST /documents" do
+    # Files
     let(:svg_file) { fixture_file_upload(Rails.root.join("spec/fixture/files/test.svg"), "images/svg+xml")}
     let(:other_file) { fixture_file_upload(Rails.root.join("spec/fixture/files/text.txt"), "text/plain")}
     let(:incorrect_svg_file) { fixture_file_upload(Rails.root.join("spec/fixture/files/incorrect.svg"), "images/svg+xml") }
     
+    # Check if controller works
     it "Returns link to converted file on success" do
       post "/documents",  params: { file: svg_file }
       expect(response).to have_http_status(:ok)
@@ -17,17 +19,19 @@ describe "Documents_API", type: :request do
       expect(json["url"]).to match(/\.pdf$/)
     end
 
-  
+    # Check if file uploaded 
     it "Return error if file not uploaded" do
       post "/documents",  params: { }
-      expect(response).to have_http_status(:bad_request)
+      expect(response).to have_http_status(:unprocessable_entity)
     end
 
+    # Check if file ext correct 
     it "Return error if file type incorrect" do
       post "/documents", params: { file: other_file }
-      expect(response).to have_http_status(:bad_request)
+      expect(response).to have_http_status(:unsupported_media_type)
     end
 
+    # Check if file uploaded have correct data
     it "Return error if svg incorrect" do
       post "/documents", params: { file: incorrect_svg_file }
       expect(response).to have_http_status(:bad_request)
