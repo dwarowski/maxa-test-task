fe406128ad1c8a2469a1f1336f3817ceb092c78b
Dwarowski <realcarenthusiast@yandex.ru>
2025-08-24 20:41:08 +0300

    refactor: documented

--- app/services/svg_to_pdf_service.rb
+++ app/services/svg_to_pdf_service.rb
@@ -1,6 +1,14 @@
 require "prawn"
 require "prawn-svg"
 
+# Convretion Service
+#
+# Converts an SVG file to PDF, adding watermark and margin.
+#
+# @param svg_content [String] SVG file content
+# @param options [Hash] Optional params hash
+# @option options [String] :watermark (DEFAULTS[:watermark]) Watermark to display
+# @option options [Integer] :margin_cm (DEFAULTS[:margin_cm]) Margin for borders
 class SvgToPdfService
   attr_reader :svg_content, :options
 
@@ -15,17 +23,18 @@ class SvgToPdfService
     @options = DEFAULTS.merge(options)
   end
 
+  # Creates new pdf add watermark, borders and svg 
   def call
-    #borders
+    # Borders
     pdf = Prawn::Document.new(
       margin: @options[:margin_cm] * 28.35
     )
 
-    #bounds
+    # Bounds
     max_width = pdf.bounds.width
     max_height = pdf.bounds.height
 
-    #svg
+    # Svg
     svg_handler = Prawn::Svg::Interface.new(
       @svg_content,
       pdf,
@@ -35,7 +44,7 @@ class SvgToPdfService
     )
     svg_handler.draw
 
-    #watermark
+    # Watermark
     pdf.transparent(0.1) do
       pdf.draw_text(
         @options[:watermark],
@@ -47,7 +56,12 @@ class SvgToPdfService
     pdf
   end
 
+  # Saves changed file to path
+  #
+  # @param [String] path Full path and filename to save pdf
+  # @return [String] file content string  
   def save(path)
+    # save pdf to determined path
     call.render_file(path)
   end
 end
