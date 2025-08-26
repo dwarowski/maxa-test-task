57b1957dce605b2c4c11b4d9a42da6a28d5b2f46
Dwarowski <realcarenthusiast@yandex.ru>
2025-08-26 13:45:02 +0300

    refactor: formatted

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  mount Rswag::Api::Engine => '/api-docs'
-  mount Rswag::Ui::Engine => '/api-docs'
-  
+  mount Rswag::Api::Engine => "/api-docs"
+  mount Rswag::Ui::Engine => "/api-docs"
+
   root "pages#home"
-  resource :documents, only: [:create]
+  resource :documents, only: [ :create ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
